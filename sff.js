451568f27e192d0f10999abc4f8c86451c9b95e4
Towhidul Islam <towhidulrafi1999@gmail.com>
2021-01-29 04:30:00 +0600

    complete

---
+++ sff.js
@@ -0,0 +1,3 @@
+let b=90;
+let a= Math.floor(0.15*b)
+console.log(a)
--- style.js
+++ style.js
@@ -1,14 +1,17 @@
-function clickChange(buttonId,displayId,costId,price,change){
+function clickChange(buttonId,displayId,costId,anotherId,price,change){
     const increasedMobileAmount = document.getElementById(buttonId);
     increasedMobileAmount.addEventListener("click",function(){
     let quantity=quantityChange(displayId,change);
-    costChange(costId,price,quantity);
+    let cost = costChange(costId,price,quantity);
+    let anotherCost = parseInt(document.getElementById(anotherId).innerText);
+    displayTotal(cost,anotherCost);
 });
 }
-clickChange("mobile-quantity-increase","quantityMobile","mobileCost",1219,+1);
-clickChange("mobile-quantity-decrease","quantityMobile","mobileCost",1219,-1);
-clickChange("casing-quantity-increase","quantityCasing","casingCost",59,+1);
-clickChange("casing-quantity-decrease","quantityCasing","casingCost",59,-1);
+let mobileCost,casingCost;
+mobileCost=clickChange("mobile-quantity-increase","quantityMobile","mobileCost","casingCost",1219,+1);
+mobileCost=clickChange("mobile-quantity-decrease","quantityMobile","mobileCost","casingCost",1219,-1);
+casingCost=clickChange("casing-quantity-increase","quantityCasing","casingCost","mobileCost",59,+1);
+casingCost=clickChange("casing-quantity-decrease","quantityCasing","casingCost","mobileCost",59,-1);
 
 
 // const increasedMobileAmount = document.getElementById("mobile-quantity-increase");
@@ -32,19 +35,35 @@ clickChange("casing-quantity-decrease","quantityCasing","casingCost",59,-1);
 // });
 
 
-
+// quantity change is done
 function quantityChange(id,change){
     let oldQuantity = document.getElementById(id).value;
     oldQuantity = parseInt(oldQuantity);
     let newQuantity = oldQuantity + change;
     if(newQuantity>=0){
         document.getElementById(id).value = newQuantity;
-        return newQuantity;
     }else{
-        alert("Quantity can not be decreased more")
+        alert("Quantity can not be decreased more");
     }
+    return newQuantity;
 }
+// total cost of one product change
 function costChange(id,price,quantity){
-    let newCost = price * quantity;
-    document.getElementById(id).innerText = newCost ;
+    let newCost=0;
+    if(quantity<=0){
+        document.getElementById(id).innerText = '0' ;
+    }else{
+        newCost = price * quantity;
+        document.getElementById(id).innerText = newCost ;
+    }
+    return newCost;
+}
+// total amount change
+function displayTotal(mobileCost,casingCost){
+    let subTotal=mobileCost+casingCost;
+    let tax= Math.floor(0.15*subTotal);
+    let total = subTotal+tax;
+    document.getElementById("Subtotal").innerText=subTotal;
+    document.getElementById("tax").innerText= tax;
+    document.getElementById("total").innerText= total;
 }
